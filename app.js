94eca61bac0b9609ef3d5a38babe83dbf0fa1bed
Mateusz Rorat <mateuszrorat@gmail.com>
2021-03-22 22:05:05 +0100

    game class and simple visuals

--- app.js
+++ app.js
@@ -11,13 +11,6 @@ app.get('/', function (req, res, next) {
   res.sendFile(__dirname + '/index.html');
 });
 
-const getTabu = async () => {
-  const result = await fetch(
-    'https://playtaboo.com/ajax/v1/next?' + new Date().getTime()
-  );
-  const resultBuffer = await result.buffer();
-  return resultBuffer.toString();
-};
 // api
 app.get('/api/tabu', async function (req, res, next) {
   const resultBuffer = await getTabu();
@@ -27,26 +20,57 @@ app.get('/api/tabu', async function (req, res, next) {
 // Users.js
 let users = [];
 const addUser = (socket) => {
+  const countRed = getTeam('red').length;
+  const countBlue = getTeam('blue').length;
+  console.log(countRed, countBlue);
+  socket.team = countBlue >= countRed ? 'red' : 'blue';
   users.push(socket);
 };
 const removeUser = (socketId) =>
   (users = users.filter((user) => user.id !== socketId));
-const getTeam = (color) => users.filter((user) => user.color === color);
+const getTeam = (color) => users.filter((user) => user.team === color);
+const getUser = (socketId) => users.find((user) => user.id === socketId);
 
-const emitTabu = () => {
+const emitTabu = (io) => {
   users.forEach((user) => {
     io.to(user.id).emit(
       'tabu',
-      user.team !== turn || users[currentUser].id === user.id ? tabu : null
+      user.team !== game.turn || users[game.currentUser].id === user.id
+        ? game.tabu
+        : null
     );
   });
 };
 
-const whoseTurn = () => {
+const emitScore = (io) => {
+  users.forEach((user) => {
+    io.to(user.id).emit('score', game.score);
+  });
+};
+
+const emitGame = (io) => {
+  users.forEach((user) => {
+    io.to(user.id).emit('game-state', game.gameInOn);
+  });
+};
+
+const emitReadiness = (io) => {
+  users.forEach((user) => {
+    user;
+    io.to(user.id).emit(
+      'readiness',
+      users.filter((user) => user.ready).length + '/' + users.length
+    );
+  });
+};
+
+const whoseTurn = (io) => {
   users.forEach((user) => {
     io.to(user.id).emit(
-      users[currentUser].id === user.id ? 'your-turn' : 'not-your-turn',
-      users[currentUser].id === user.id ? rundLength : users[currentUser].team
+      users[game.currentUser].id === user.id ? 'your-turn' : 'not-your-turn',
+      users[game.currentUser].id === user.id
+        ? game.roundLength
+        : users[game.currentUser].team
     );
   });
 };
@@ -59,32 +83,102 @@ io.on('connection', function (socket) {
   });
   socket.on('join', function (name) {
     addUser(socket);
+    socket.ready = false;
     io.to(socket.id).emit('team-assignment', socket.team);
-    emitTabu();
-    whoseTurn();
+    emitReadiness(io);
+    emitGame(io);
   });
   socket.on('skip', async function () {
-    tabu = await getTabu();
-    emitTabu();
+    await game.nextTabu();
+    emitTabu(io);
+  });
+  socket.on('ready', async function () {
+    socket.ready = true;
+    if (users.every((user) => user.ready)) {
+      game.start();
+    }
+    emitReadiness(io);
+  });
+  socket.on('ok', async function (socketId) {
+    game.updateScore(getUser(socketId).team, 1);
+    await game.nextTabu();
+    emitTabu(io);
+    emitScore(io);
+  });
+  socket.on('wrong', async function (socketId) {
+    game.updateScore(getUser(socketId).team === 'red' ? 'blue' : 'red', -1);
+    await game.nextTabu();
+    emitTabu(io);
+    emitScore(io);
   });
 });
 
 //Game.js
-const rundLength = 121;
-let currentUser = 0;
-let turn = 'blue';
-const timer = setInterval(() => {
-  nextUser();
-}, 1000 * rundLength);
+class Game {
+  round = 0;
+  currentUser = -1;
+  roundLength;
+  teams;
+  score = {};
+  timer;
+  tabu;
+  io;
+  gameInOn;
 
-const nextUser = async () => {
-  tabu = await getTabu();
-  currentUser = ++currentUser % users.length;
-  turn = users[currentUser].team;
-  emitTabu();
-  whoseTurn();
-};
+  constructor(roundLength, teams, io) {
+    this.gameInOn = false;
+    emitGame(io);
+    this.nextTabu().then((res) => {
+      this.roundLength = roundLength;
+      teams.forEach((team) => (this.score[team] = 0));
+      this.io = io;
+    });
+  }
+  async nextUser() {
+    await game.nextTabu();
+    this.currentUser = ++this.currentUser % users.length;
+    this.turn = users[game.currentUser].team;
+    emitTabu(this.io);
+    whoseTurn(this.io);
+  }
 
-let tabu = null;
-getTabu().then((res) => (tabu = res));
+  updateScore(team, points) {
+    this.score[team] = this.score[team] + points;
+  }
+
+  start() {
+    this.gameInOn = true;
+    console.log('game start');
+    this.nextUser();
+    emitGame(io);
+    this.timer = setInterval(() => {
+      this.nextUser();
+      emitTabu(this.io);
+      whoseTurn(this.io);
+      emitScore(this.io);
+    }, 1000 * this.roundLength);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+    this.gameInOn = false;
+    emitGame(io);
+  }
+
+  async nextTabu() {
+    this.tabu = await this.getTabu();
+    return this.tabu;
+  }
+
+  async getTabu() {
+    const result = await fetch(
+      'https://playtaboo.com/ajax/v1/next?' + new Date().getTime()
+    );
+    const resultBuffer = await result.buffer();
+    return resultBuffer.toString();
+  }
+}
+
+const game = new Game(120, ['blue', 'red'], io);
+
 server.listen(4200);
